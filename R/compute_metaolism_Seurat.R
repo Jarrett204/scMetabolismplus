69cc53bcf0b704c0b16892af0b309b1c9880a9ab
Jarrett204 <liojarrett@gmail.com>
2024-03-15 15:57:23 +0800

    Initial commit

---
+++ R/compute_metaolism_Seurat.R
@@ -0,0 +1,100 @@
+#' scMetabolismplus
+#'
+#' scMetabolism
+#' @param obj seruat 对象
+#' @param Cancer 数据属于的癌种 对象
+#' @keywords scMetabolismplus
+#' @examples
+#' sc.metabolism.Seurat()
+#' @export sc.metabolism.Seurat
+
+
+sc.metabolism.Seurat <- function(obj, method = "AUCell", imputation = F,Cancer="BRCA", metabolism.type = "KEGG") {
+
+  countexp<-obj@assays$RNA@counts
+
+  countexp<-data.frame(as.matrix(countexp))
+
+  #signatures_KEGG_metab <- "./data/KEGG_metabolism_nc.gmt"
+  #signatures_REACTOME_metab <- "./data/REACTOME_metabolism.gmt"
+
+  signatures_KEGG_metab <- system.file("data/KEGG/", paste0(Cancer,".gmt"), package = "scMetabolismplus")
+  signatures_REACTOME_metab <- system.file("data/Reactome/", paste0(Cancer,".gmt"), package = "scMetabolismplus")
+  signatures_GO_metab <- system.file("data/GO/", paste0(Cancer,".gmt"), package = "scMetabolismplus")
+  signatures_HALLMARK_metab <- system.file("data/Hallmark/", paste0(Cancer,".gmt"), package = "scMetabolismplus")
+
+
+  if (metabolism.type == "KEGG")  {gmtFile<-signatures_KEGG_metab; cat("Your choice is: KEGG\n")}
+  if (metabolism.type == "REACTOME")  {gmtFile<-signatures_REACTOME_metab; cat("Your choice is: REACTOME\n")}
+  if (metabolism.type == "GO")  {gmtFile<-signatures_GO_metab; cat("Your choice is: GO\n")}
+  if (metabolism.type == "Hallmark")  {gmtFile<-signatures_HALLMARK_metab; cat("Your choice is: Hallmark\n")}
+
+  #imputation
+  if (imputation == F) {
+    countexp2<-countexp
+  }
+  if (imputation == T) {
+
+    cat("Start imputation...\n")
+
+    #Citation: George C. Linderman, Jun Zhao, Yuval Kluger. Zero-preserving imputation of scRNA-seq data using low-rank approximation. bioRxiv. doi: https://doi.org/10.1101/397588
+    #Github: https://github.com/KlugerLab/ALRA
+
+    cat("Citation: George C. Linderman, Jun Zhao, Yuval Kluger. Zero-preserving imputation of scRNA-seq data using low-rank approximation. bioRxiv. doi: https://doi.org/10.1101/397588 \n")
+
+
+    result.completed <- alra(as.matrix(countexp))
+    countexp2 <- result.completed[[3]]; row.names(countexp2) <- row.names(countexp)
+  }
+
+  #signature method
+  cat("Start quantify the metabolism activity...\n")
+
+  #VISION 暂时看不好使
+  #if (method == "VISION") {
+  #  library(VISION)
+  #  n.umi <- colSums(countexp2)
+  #  scaled_counts <- t(t(countexp2) / n.umi) * median(n.umi)
+  #  vis <- Vision(scaled_counts, signatures = gmtFile)
+
+  #  options(mc.cores = ncores)
+
+  #  vis <- analyze(vis)
+
+   # signature_exp<-data.frame(t(vis@SigScores))
+  #}
+
+  #AUCell
+  if (method == "AUCell") {
+    library(AUCell)
+    library(GSEABase)
+    cells_rankings <- AUCell_buildRankings(as.matrix(countexp2), plotStats=F) #rank
+    geneSets <- getGmt(gmtFile) #signature read
+    cells_AUC <- AUCell_calcAUC(geneSets, cells_rankings) #calc
+    signature_exp <- data.frame(getAUC(cells_AUC))
+  }
+
+  #ssGSEA
+  if (method == "ssGSEA") {
+    library(GSVA)
+    library(GSEABase)
+    geneSets <- getGmt(gmtFile) #signature read
+    gsva_es <- gsva(as.matrix(countexp2), geneSets, method=c("ssgsea"), kcdf=c("Poisson"), parallel.sz=ncores) #
+    signature_exp<-data.frame(gsva_es)
+  }
+
+  #GSVA
+  if (method == "ssGSEA") {
+    library(GSVA)
+    library(GSEABase)
+    geneSets <- getGmt(gmtFile) #signature read
+    gsva_es <- gsva(as.matrix(countexp2), geneSets, method=c("gsva"), kcdf=c("Poisson"), parallel.sz=ncores) #
+    signature_exp<-data.frame(gsva_es)
+  }
+
+  cat("\nPlease Cite: \nYingcheng Wu, Qiang Gao, et al. Cancer Discovery. 2021. \nhttps://pubmed.ncbi.nlm.nih.gov/34417225/   \n\n")
+
+  obj@assays$METABOLISM$score<-signature_exp
+  obj
+}
+
---
+++ R/visualize_metabolism.R
@@ -0,0 +1,218 @@
+#' scMetabolism
+#'
+#' scMetabolism
+#' @param obj objective 
+#' @param pathway objective pathway
+#' @param phenotype phenotype
+#' @keywords scMetabolism
+#' @examples
+#' DimPlot.metabolism()
+#' DotPlot.metabolism()
+#' BoxPlot.metabolism()
+#' @export DimPlot.metabolism
+#' @export DotPlot.metabolism
+#' @export BoxPlot.metabolism
+
+
+library(ggplot2)
+library(wesanderson)
+library(data.table)
+
+DimPlot.metabolism <- function(obj, pathway, dimention.reduction.type = "umap", dimention.reduction.run = T, size= 1){
+  
+  cat("\nPlease Cite: \nYingcheng Wu, Qiang Gao, et al. Cancer Discovery. 2021. \nhttps://pubmed.ncbi.nlm.nih.gov/34417225/   \n\n")
+
+  #umap
+  if (dimention.reduction.type == "umap"){
+
+    if (dimention.reduction.run == T) obj <- Seurat::RunUMAP(obj, reduction = "pca", dims = 1:40)
+    umap.loc<-obj@reductions$umap@cell.embeddings
+
+    row.names(umap.loc)<-colnames(obj)
+    signature_exp<-obj@assays$METABOLISM$score
+
+    input.pathway <- pathway
+
+    signature_ggplot<-data.frame(umap.loc, t(signature_exp[input.pathway,]))
+
+    library(wesanderson)
+    pal <- wes_palette("Zissou1", 100, type = "continuous")
+
+
+    library(ggplot2)
+    plot <- ggplot(data=signature_ggplot, aes(x=UMAP_1, y=UMAP_2, color = signature_ggplot[,3])) +  #this plot is great
+      geom_point(size = size) +
+      scale_fill_gradientn(colours = pal) +
+      scale_color_gradientn(colours = pal) +
+      labs(color = input.pathway) +
+      #xlim(0, 2)+ ylim(0, 2)+
+      xlab("UMAP 1") +ylab("UMAP 2") +
+      theme(aspect.ratio=1)+
+      #theme_bw()
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+            panel.background = element_blank(), axis.line = element_line(colour = "black"))
+  }
+
+  #tsne
+  if (dimention.reduction.type == "tsne"){
+    if (dimention.reduction.run == T) obj <- Seurat::RunTSNE(obj, reduction = "pca", dims = 1:40)
+    tsne.loc<-obj@reductions$tsne@cell.embeddings
+
+    row.names(tsne.loc)<-colnames(obj)
+    signature_exp<-obj@assays$METABOLISM$score
+
+    input.pathway <- pathway
+
+    signature_ggplot<-data.frame(tsne.loc, t(signature_exp[input.pathway,]))
+
+    pal <- wes_palette("Zissou1", 100, type = "continuous")
+
+
+    library(ggplot2)
+    plot <- ggplot(data=signature_ggplot, aes(x=tSNE_1, y=tSNE_2, color = signature_ggplot[,3])) +  #this plot is great
+      geom_point(size = size) +
+      scale_fill_gradientn(colours = pal) +
+      scale_color_gradientn(colours = pal) +
+      labs(color = input.pathway) +
+      #xlim(0, 2)+ ylim(0, 2)+
+      xlab("tSNE 1") +ylab("tSNE 2") +
+      theme(aspect.ratio=1)+
+      #theme_bw()
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+            panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+
+  }
+  plot
+}
+
+
+DotPlot.metabolism <- function(obj, pathway, phenotype, norm = "y"){
+  input.norm = norm
+  input.pathway <- pathway
+  input.parameter<-phenotype
+
+  metadata<-obj@meta.data
+  metabolism.matrix <- obj@assays$METABOLISM$score
+
+  cat("\nPlease Cite: \nYingcheng Wu, Qiang Gao, et al. Cancer Discovery. 2021. \nhttps://pubmed.ncbi.nlm.nih.gov/34417225/   \n\n")
+
+
+  metadata[,input.parameter]<-as.character(metadata[,input.parameter])
+  metabolism.matrix_sub<-t(metabolism.matrix[input.pathway,])
+
+  #arrange large table
+  gg_table<-c()
+  for (i in 1:length(input.pathway)){
+    gg_table<-rbind(gg_table, cbind(metadata[,input.parameter], input.pathway[i], metabolism.matrix_sub[,i]))
+  }
+  gg_table<-data.frame(gg_table)
+
+  #get median value
+  gg_table_median<-c()
+  input.group.x<-unique(as.character(gg_table[,1]))
+  input.group.y<-unique(as.character(gg_table[,2]))
+
+
+  for (x in 1:length(input.group.x)){
+    for (y in 1:length(input.group.y)){
+      gg_table_sub<-subset(gg_table, gg_table[,1] == input.group.x[x] & gg_table[,2] == input.group.y[y])
+      gg_table_median<-rbind(gg_table_median, cbind(input.group.x[x], input.group.y[y], median(as.numeric(as.character(gg_table_sub[,3])))))
+
+    }
+  }
+  gg_table_median<-data.frame(gg_table_median)
+  gg_table_median[,3]<-as.numeric(as.character(gg_table_median[,3]))
+
+
+  #normalize
+  gg_table_median_norm<-c()
+  input.group.x<-unique(as.character(gg_table[,1]))
+  input.group.y<-unique(as.character(gg_table[,2]))
+
+
+  range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+  if (input.norm == "y")
+    for (y in 1:length(input.group.y)){
+      gg_table_median_sub<-subset(gg_table_median, gg_table_median[,2] == input.group.y[y])
+      norm_value<- range01(as.numeric(as.character(gg_table_median_sub[,3])))
+      gg_table_median_sub[,3]<-norm_value
+      gg_table_median_norm<-rbind(gg_table_median_norm, gg_table_median_sub)
+    }
+
+  if (input.norm == "x")
+    for (x in 1:length(input.group.x)){
+      gg_table_median_sub<-subset(gg_table_median, gg_table_median[,1] == input.group.x[x])
+      norm_value<- range01(as.numeric(as.character(gg_table_median_sub[,3])))
+      gg_table_median_sub[,3]<-norm_value
+      gg_table_median_norm<-rbind(gg_table_median_norm, gg_table_median_sub)
+    }
+
+  if (input.norm == "na") gg_table_median_norm<-gg_table_median
+
+
+  gg_table_median_norm<-data.frame(gg_table_median_norm)
+  gg_table_median_norm[,3]<-as.numeric(as.character(gg_table_median_norm[,3]))
+
+
+
+
+  library(wesanderson)
+  pal <- wes_palette("Zissou1", 100, type = "continuous")
+
+  ggplot(data=gg_table_median_norm, aes(x=gg_table_median_norm[,1], y=gg_table_median_norm[,2], color = gg_table_median_norm[,3])) +
+    geom_point(data=gg_table_median_norm, aes(size = gg_table_median_norm[,3])) + #geom_line() +
+    #theme_bw()+theme(aspect.ratio=0.5, axis.text.x = element_text(angle = 45, hjust = 1)) +
+    ylab("Metabolic Pathway")+ xlab(input.parameter)+
+    theme_bw()+theme(axis.text.x = element_text(angle = 45, hjust = 1), #aspect.ratio=1,
+                     panel.grid.minor = element_blank(), panel.grid.major = element_blank()) +
+    scale_color_gradientn(colours = pal) +
+    labs(color = "Value", size = "Value") +
+    #facet_wrap(~tissueunique, ncol = 1) +
+    #theme_bw()+theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    NULL
+}
+
+
+BoxPlot.metabolism <- function(obj, pathway, phenotype, ncol = 1){
+  input.pathway<-pathway
+  input.parameter<-phenotype
+
+  cat("\nPlease Cite: \nYingcheng Wu, Qiang Gao, et al. Cancer Discovery. 2021. \nhttps://pubmed.ncbi.nlm.nih.gov/34417225/   \n\n")
+
+  metadata<-obj@meta.data
+  metabolism.matrix <- obj@assays$METABOLISM$score
+
+
+
+  metadata[,input.parameter]<-as.character(metadata[,input.parameter])
+  metabolism.matrix_sub<-t(metabolism.matrix[input.pathway,])
+
+  #arrange large table
+  gg_table<-c()
+  for (i in 1:length(input.pathway)){
+    gg_table<-rbind(gg_table, cbind(metadata[,input.parameter], input.pathway[i], metabolism.matrix_sub[,i]))
+  }
+  gg_table<-data.frame(gg_table)
+  gg_table[,3]<-as.numeric(as.character(gg_table[,3]))
+
+
+  library(wesanderson)
+  pal <- wes_palette("Zissou1", 100, type = "continuous")
+
+  ggplot(data=gg_table, aes(x=gg_table[,1], y=gg_table[,3], fill = gg_table[,1])) +
+    geom_boxplot(outlier.shape=NA)+
+    ylab("Metabolic Pathway")+
+    xlab(input.parameter)+
+    theme_bw()+theme(axis.text.x = element_text(angle = 45, hjust = 1), #aspect.ratio=1,
+                     panel.grid.minor = element_blank(), panel.grid.major = element_blank()) +
+    #scale_color_gradientn(colours = pal) +
+    facet_wrap(~gg_table[,2], ncol = ncol, scales = "free") +
+    labs(fill = input.parameter) +
+    #theme_bw()+theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    NULL
+}
+
+
+
